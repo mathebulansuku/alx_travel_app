e492bd3c995c53c434c4de3be3a300f71d834ae9
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-05-25 18:25:15 +0200

    week 2 milestone BE complete

---
+++ alx_travel_app/settings.py
@@ -0,0 +1,16 @@
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
